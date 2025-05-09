5c98aa050fdc3f36b2e5d3f821b0236bb90f8b56
Julian <juli2003juli@gmail.com>
2025-05-09 17:05:12 +0300

    Contact form is done

---
+++ src/scripts/script-about-contact.js
@@ -0,0 +1,24 @@
+//Buttons
+const buttonAbout = document.querySelector('.about');
+const buttonContact = document.querySelector('.contact');
+
+//Dialogs
+const dialogAbout = document.querySelector('.dialog-about');
+const dialogContact = document.querySelector('.dialog-contact');
+
+//country container
+const country = document.querySelector('.section-country-info')
+
+buttonAbout.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    country.style.display = 'none';
+    dialogAbout.open = true;
+})
+
+buttonContact.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    country.style.display = 'none';
+    dialogContact.open = true;
+})
