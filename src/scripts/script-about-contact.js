27393d4180d319bd9454338df374ed2508c63ff1
Julian <juli2003juli@gmail.com>
2025-05-09 17:17:29 +0300

    script that closes a dialog if another dialog wants to be open

--- src/scripts/script-about-contact.js
+++ src/scripts/script-about-contact.js
@@ -7,12 +7,14 @@ const dialogAbout = document.querySelector('.dialog-about');
 const dialogContact = document.querySelector('.dialog-contact');
 
 //country container
-const country = document.querySelector('.section-country-info')
+const country = document.querySelector('.section-country-info');
+
 
 buttonAbout.addEventListener('click', (e) => {
     e.preventDefault();
 
     country.style.display = 'none';
+    dialogContact.open = false;
     dialogAbout.open = true;
 })
 
@@ -20,5 +22,6 @@ buttonContact.addEventListener('click', (e) => {
     e.preventDefault();
 
     country.style.display = 'none';
+    dialogAbout.open = false;
     dialogContact.open = true;
 })
