a0c814369fad31dedc757e6b9ddcc3bdbd063c54
Julian <juli2003juli@gmail.com>
2025-05-11 23:45:20 +0300

    message opacity 0

--- src/scripts/script-about-contact.js
+++ src/scripts/script-about-contact.js
@@ -9,6 +9,16 @@ const dialogContact = document.querySelector('.dialog-contact');
 //country container
 const country = document.querySelector('.section-country-info');
 
+//Central message
+const centralMessage = document.querySelector('.message');
+
+setTimeout(() => {
+    centralMessage.style.opacity = '0'
+}, 0)
+
+setTimeout(() => {
+    centralMessage.style.display = 'none'
+}, 8000)
 
 buttonAbout.addEventListener('click', (e) => {
     e.preventDefault();
@@ -16,6 +26,9 @@ buttonAbout.addEventListener('click', (e) => {
     country.style.display = 'none';
     dialogContact.open = false;
     dialogAbout.open = true;
+
+    centralMessage.style.display = 'none'
+
 })
 
 buttonContact.addEventListener('click', (e) => {
@@ -24,4 +37,8 @@ buttonContact.addEventListener('click', (e) => {
     country.style.display = 'none';
     dialogAbout.open = false;
     dialogContact.open = true;
+
+    centralMessage.style.display = 'none'
+
 })
+
